9b7d2df63f5b2b18720d08a3d0e2981ccb7a8fdd
Thomas Doherty <thomas@selfsustain.com>
2018-03-24 15:07:24 -0700

    Work with jquery.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//Business Logic
+function Pizza (size, topping, name) {
+  console.log(size);
+  console.log(topping);
+  this.name = name;
+  this.size = size;
+  this.topping = topping;
+  this.setPizzaPrice();
+}
+
+Pizza.prototype.setPizzaPrice = function(){
+  return this.size + this.topping;
+}
+
+//User Interface
+$(document).ready(function() {
+
+  $("#submit").click(function(event){
+    alert("submit");
+    event.preventDefault();
+    var selectedSize = parseInt($("select#pizza-size").val());
+    var selectedTopping = parseInt($("select#pizza-type").val());
+    var name = $("#firstname").val();
+    //var selectedToppings = $("input:checkbox[name=topping]:checked").val();
+    console.log(selectedTopping);
+    console.log(selectedSize);
+    console.log(name);
+    var pizza = new Pizza(selectedSize, selectedTopping, name);
+
+    console.log(pizza);
+
+    $("#your-order").append("<li>" + pizza.name + " Your order will cost $" + pizza.setPizzaPrice() + "</li>");
+  });
+});
